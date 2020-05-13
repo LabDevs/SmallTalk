5e71048bda072b2280b03f2c09c580ef7d3f51c0
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-05-13 18:53:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const router = require('./routes/user')
 
 const app = express()
 const port = process.env.PORT || 3000
-const hostName = '127.0.0.1'
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -16,4 +15,4 @@ app.use(router)
 
 app.get('/', (req, res) => res.send('Hello World'))
 
-app.listen(port, hostName, () => console.log(`Listening on port ${port} `))
+app.listen(port, () => console.log(`Listening on port ${port} `))
