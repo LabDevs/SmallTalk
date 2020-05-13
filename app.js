a04f843d726a3c19230c7e091c6d4a7e86321579
paulsobers23 <paulsobers23@gmail.com>
2020-05-13 18:26:25 -0400

    fixed merge conflict

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express')
+
+const bodyParser = require('body-parser')
+
+const router = require('./routes/user')
+
 const app = express()
 const port = process.env.PORT || 3000
+const hostName = '127.0.0.1'
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(router)
 
 app.get('/', (req, res) => res.send('Hello World'))
 
-app.listen(port, () => console.log(`Listening on port ${port} `))
+app.listen(port,hostName, () => console.log(`Listening on port ${port} `))
