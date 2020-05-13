33635fc86828c7991e3eb8a04f2db7c537423b86
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-05-13 11:43:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-const hostName = '127.0.0.1'
-
 app.get('/', (req, res) => res.send('Hello World'))
 
-app.listen(port, hostName, () => console.log(`Listening on port ${port} `))
+app.listen(port, () => console.log(`Listening on port ${port} `))
