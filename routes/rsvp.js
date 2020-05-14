85780d3e2e68942709f262529ccc7356068b2052
Stephanie <stephaniesmith12514@gmail.com>
2020-05-14 13:40:47 -0400

    Adds routing for RSVP's

---
+++ routes/rsvp.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const rsvpController = require('../controllers/rsvp')
+const router = express.Router()
+
+router.post('/rsvp', rsvpController.add)
+
+router.get('/rsvp/user', rsvpController.getByUser)
+
+router.module.exports = router
