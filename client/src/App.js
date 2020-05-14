ae50e47187a853bfd53b528666efa9e2d2351829
paulsobers23 <paulsobers23@gmail.com>
2020-05-14 18:08:09 -0400

    got react app up and running and started nav bar back bone

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React from "react";
+import { Navbar, Nav} from "react-bootstrap";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello
-        </p>
-      </header>
-    </div>
+    <Router>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            src="https://via.placeholder.com/150"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="placeholder"
+          />
+        </Navbar.Brand>
+  
+        <Nav className="mr.auto">
+          <Link to="/home">Dash</Link>
+          <Link to="/categories">Topics</Link>
+        </Nav>
+      </Navbar>
+        
+      <Switch>
+        <Route path="/:id" children={<RouteList />} />
+      </Switch>
+    </Router>
   );
+}
+
+// will refactor this to add more routes when needed
+function RouteList(){
+  let {id} = useParams();
+  return (
+    <div>
+      <p>Access to route id {id}</p>
+    </div>
+    )
 }
 
 export default App;
