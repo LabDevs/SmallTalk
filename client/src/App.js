d7c54bbaad44ec7a7777f257297f1f520c5026ca
paulsobers23 <paulsobers23@gmail.com>
2020-05-16 19:14:22 -0400

    fixed hopefully

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 // import { Navbar, Nav } from 'react-bootstrap'
 import {
   BrowserRouter as Router,
@@ -8,94 +8,58 @@ import {
   Redirect,
   useParams,
 } from "react-router-dom";
-import CategoriesContextProvider from './contexts/CategoriesContextProvider'
-import CategoriesEventList from './components/CategoriesEventList'
-import Register from './components/Register'
-import Login from './components/Login'
-import Home from './components/Home'
-import AddEvent from './components/AddEvent'
-import UpdateEvent from './components/UpdateEvent'
+import CategoriesContextProvider from "./contexts/CategoriesContextProvider";
+import CategoriesEventList from "./components/CategoriesEventList";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import AddEvent from "./components/AddEvent";
+import UpdateEvent from "./components/UpdateEvent";
 
 function App() {
   return (
     <Router>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            src="https://via.placeholder.com/150"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="placeholder"
+          />
+        </Navbar.Brand>
+
+        <Nav className="mr.auto">
+          <Link to="/dash">Dash</Link>
+          <Link to="/categories">Topics</Link>
+        </Nav>
+      </Navbar>
+
       <Switch>
-        <Route path="/api/register">
-          <RouteList/>
-        </Route>
-        <Route path="/api/login" >
-          <Login />
-        </Route>
-        </Switch>
-        
-        <PrivateRoute path="/api/getEvents">
-          <DashBoard />
-        </PrivateRoute>
-        
-      <Switch>
-        <Route exact path='/'>
+        <Route exact path="/">
           <Home />
         </Route>
-        <Route path='/register'>
+        <Route path="/register">
           <Register />
         </Route>
-        <Route path='/addEvent'>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/addEvent">
           <AddEvent />
         </Route>
-        <Route path='/updateEvent'>
+        <Route path="/updateEvent">
           <UpdateEvent />
         </Route>
-        <Route path='/categories/:id'>
+        <Route path="/categories/:id">
           <CategoriesContextProvider>
             <CategoriesEventList />
           </CategoriesContextProvider>
         </Route>
       </Switch>
     </Router>
-
-  )
-}
-
-// will refactor this to add more routes when needed
-function RouteList() {
-  let { id } = useParams();
-  return (
-    <div>
-      <p>Access to route id {id}</p>
-    </div>
   );
 }
 
-
-function PrivateRoute({children}){
-  return (
-    <Route>
-      <Redirect
-        to={{pathname:"/api/login"}}/>
-    </Route>
-    )
-}
-
-function DashBoard(){
-  return <h1>DashBoard</h1>
-}
-
-// <Navbar bg='dark' variant='dark'>
-//         <Navbar.Brand href='#home'>
-//           <img
-//             src='https://via.placeholder.com/150'
-//             width='30'
-//             height='30'
-//             className='d-inline-block align-top'
-//             alt='placeholder'
-//           />
-//         </Navbar.Brand>
-
-//         <Nav className='mr.auto'>
-//           <Link to='/dash'>Dash</Link>
-//           <Link to='/categories'>Topics</Link>
-//         </Nav>
-//       </Navbar>
-
-export default App
-
+export default App;
