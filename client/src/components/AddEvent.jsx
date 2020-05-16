63b047b32a236cb12683727bb919de823e117095
Stephanie <stephaniesmith12514@gmail.com>
2020-05-16 18:34:56 -0400

    Updates AddEvent component to take categories

--- client/src/components/AddEvent.jsx
+++ client/src/components/AddEvent.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { Redirect } from 'react-router-dom'
 
 const AddEvent = () => {
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
+  const [categoryData, setCategoryData] = useState(null)
+  const [categoryId, setCategoryId] = useState(null)
   const [redirect, setRedirect] = useState(false)
   const eventInfo = {
-    title: title.toString(),
-    description: description.toString()
+    title: title,
+    description: description,
+    categoryId: categoryId
   }
 
   const addEvent = (e) => {
@@ -21,6 +24,16 @@ const AddEvent = () => {
       .then(() => setRedirect(true))
       .catch((err) => console.log(err))
   }
+
+  useEffect(() => {
+    async function getCategories() {
+      const response = await fetch('/api/categories')
+      const data = await response.json()
+      setCategoryData(data)
+    }
+
+    getCategories()
+  }, [])
 
   return (
     <div>
@@ -40,6 +53,14 @@ const AddEvent = () => {
             <Form.Group controlId='descriptionForm'>
               <Form.Label>Description</Form.Label>
               <Form.Control onChange={e => setDescription(e.target.value)} as="textarea" rows="3" placeholder='Give a short description of your event!' />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlSelect1">
+              <Form.Label>Category</Form.Label>
+              <Form.Control onChange={(e) => setCategoryId(e.target.value)} as="select">
+                {categoryData && categoryData.forEach(category => {
+                  <option value={category.category_id}>{category.name}</option>
+                })}
+              </Form.Control>
             </Form.Group>
             <Button
               onClick={addEvent}
