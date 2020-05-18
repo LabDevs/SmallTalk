c9c9fe40509aa5d7b4b64e0deab3dcd67c43d35b
Stephanie <stephaniesmith12514@gmail.com>
2020-05-18 12:46:55 -0400

    Fixes knex file syntax

--- knex.js
+++ knex.js
@@ -1,19 +1,14 @@
-const path = require('path');
-require('dotenv').config();
+const path = require('path')
+require('dotenv').config()
 
 module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.DEV_DB_HOST,
-      port: process.env.DEV_DB_PORT,
-      user: process.env.DEV_DB_USER,
-      password: process.env.DEV_DB_PSWD,
-      database: process.env.DEV_DB_NAME,
-<<<<<<< HEAD
+      user: process.env.USER,
+      host: process.env.HOST,
+      database: process.env.DATABASE,
+      port: process.env.DB_PORT
     }
   }
 }
-=======
-    },
->>>>>>> Adds knex, fixes issue with modals
