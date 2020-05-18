9eaa1b85b073546ad5c2ace62db73ec43e2b2fe4
Stephanie <stephaniesmith12514@gmail.com>
2020-05-18 16:23:02 -0400

    Adds knex migrations & seed for categories

--- knex.js
+++ knex.js
@@ -1,14 +1,3 @@
-const path = require('path')
-require('dotenv').config()
-
-module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
-      user: process.env.USER,
-      host: process.env.HOST,
-      database: process.env.DATABASE,
-      port: process.env.DB_PORT
-    }
-  }
-}
+const environment = process.env.ENVIRONMENT || 'development'
+const config = require('./knexfile')[environment]
+module.exports = require('knex')(config)
