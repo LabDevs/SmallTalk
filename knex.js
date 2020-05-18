5006621458b3d9181daafb019b4af4469baa5712
Stephanie <stephaniesmith12514@gmail.com>
2020-05-18 12:46:55 -0400

    Fixes knex file syntax

--- knex.js
+++ knex.js
@@ -5,11 +5,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.DEV_DB_HOST,
-      port: process.env.DEV_DB_PORT,
-      user: process.env.DEV_DB_USER,
-      password: process.env.DEV_DB_PSWD,
-      database: process.env.DEV_DB_NAME
+      user: process.env.USER,
+      host: process.env.HOST,
+      database: process.env.DATABASE,
+      port: process.env.DB_PORT
     }
   }
 }
