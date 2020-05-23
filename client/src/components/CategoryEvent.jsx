ca14b27ea3aae45b8a155af607b238c05f26be84
LaishaCalalpa <lcalalpa@gmail.com>
2020-05-22 20:32:43 -0400

    commit files

--- client/src/components/CategoryEvent.jsx
+++ client/src/components/CategoryEvent.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Card, Button } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
 
 const CategoryEvent = (props) => {
   console.log(props)
