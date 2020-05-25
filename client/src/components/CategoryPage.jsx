e5756152646ac61b817d4495aa38185244b96568
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-05-24 20:19:52 -0400

    Fixes events by category cards

--- client/src/components/CategoryPage.jsx
+++ client/src/components/CategoryPage.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { CardGroup } from 'react-bootstrap'
 import CategoryEvent from './CategoryEvent'
 import { useParams } from 'react-router-dom'
+import { Grid, Box, Heading } from 'grommet'
 
 const CategoryPage = () => {
   const { categoryId } = useParams()
-  const [categoryEvents, setCategoryEvents] = useState(null)
+  const [categoryEvents, setCategoryEvents] = useState([])
 
   // This function will take in a categoryId, which will be provided by the Categories/Topics component as
   // a prop since that component will be making a fetch request to get all categories, which includes the id.
@@ -14,16 +14,32 @@ const CategoryPage = () => {
       const response = await fetch(`/api/categories/${categoryId}`)
       const json = await response.json()
       setCategoryEvents(json)
-      console.log(categoryEvents)
     }
     getEventsByCategory()
   }, [])
 
+  console.log(categoryEvents)
+
   return (
-    <CardGroup className='cards'>
-      {categoryEvents &&
-        categoryEvents.map(event => <CategoryEvent event={event} />)}
-    </CardGroup>
+    <Box>
+      {categoryEvents ? (
+        <Heading text-align='center'>{categoryEvents['name']}</Heading>
+      ) : (
+        <></>
+      )}
+      <Box direction='column'>
+        <Grid
+          rows={'medium'}
+          columns={['auto', '1/2']}
+          gap='small'
+          responsive='true'
+          align='center'
+        >
+          {categoryEvents &&
+            categoryEvents.map(event => <CategoryEvent event={event} />)}
+        </Grid>
+      </Box>
+    </Box>
   )
 }
 
