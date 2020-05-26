62245b5f954fa7292b70921020025ca6164c35ed
paulsobers23 <paulsobers23@gmail.com>
2020-05-26 13:53:14 -0400

    trying to sginal the other peers when a client is in the room

--- client/src/components/VideoChatTwo.jsx
+++ client/src/components/VideoChatTwo.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom'
-import { Button, Box, Heading, Text } from 'grommet'
 import io from "socket.io-client";
 import SimplePeer from 'simple-peer'
 import styled from "styled-components";
@@ -23,74 +22,33 @@ const Video = styled.video`
   height: 50%;
 `;
 
-const socket = io()
 function VideoChatTwo() {
-  const [yourID, setYourID] = useState("");
-  const [users, setUsers] = useState({});
   const [stream, setStream] = useState();
-  const [receivingCall, setReceivingCall] = useState(false);
-  const [caller, setCaller] = useState("");
-  const [callerSignal, setCallerSignal] = useState();
-  const [callAccepted, setCallAccepted] = useState(false);
-  const [room,setRoom] = useState([])
+  const [isPartnerHere, setIsPartnerHere] = useState(null)
+  const [peer, setPeer] = useState(new SimplePeer({
+    initiator:false,
+    trickle:false,
+    stream,
+  }))
   const roomID = useParams()
 
   const userVideo = useRef();
   const partnerVideo = useRef();
 
-  //check to see if anyone is in the room
   useEffect(() => {
-    socket.emit('video-room', roomID)
     navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
       setStream(stream);
       if (userVideo.current) {
         userVideo.current.srcObject = stream;
       }
     })
-
-    socket.on("yourID", (id) => {
-      console.log(id)
-      setYourID(id);
-
-    })
-    socket.on("allUsers", (users) => {
-      setUsers(users);
-    })
     
-    socket.on('is-partner-here', callPeer)
-  
-
-    socket.on("call", (data) => {
-      console.log('call event', data)
-      setReceivingCall(true);
-      setCaller(data.from);
-      setCallerSignal(data.signal);
-    })
-    
-    socket.on("disconnect", (data) =>{
-      // console.log('user left', data)
-      // socket.emit('user left', data)
-    })
-    function getUsers(){
-      fetch('/api/users')
-        .then(res => res.json())
-        .then(data => console.log(data))
-    }
-    
-    // getUsers()
-  }, []);
-  
-
-  function callPeer(id) {
-    const peer = new SimplePeer({
-      initiator: true,
-      trickle: false,
-      stream: stream,
-    });
+    const socket = io()
+    socket.emit('video-room', roomID)
 
     peer.on("signal", (data) => {
       console.log('Data was just sent',data)
-      socket.emit("callUser", { userToCall: id, signalData: data, from: yourID })
+      socket.emit("signal", data)
     })
 
     peer.on("stream", (stream) => {
@@ -99,36 +57,30 @@ function VideoChatTwo() {
       }
     });
     
-    
-    socket.on("callAccepted", (signal) => {
+    socket.on("signal", (signal) => {
       console.log('I got the signal', signal)
-      setCallAccepted(true);
       peer.signal(signal);
     })
     
-  }
-  
-
-
-  function acceptCall() {
-    setCallAccepted(true);
-    const peer = new SimplePeer({
-      initiator: false,
-      trickle: false,
-      stream: stream,
-    });
-    peer.on("signal", data => {
-      console.log('trying to signal', data)
-      socket.emit("acceptCall", { signal: data, to: caller })
+    socket.on('is-partner-here', (isPartnerHere) => {
+      console.log('partner is here',isPartnerHere)
+      setIsPartnerHere(isPartnerHere)
+      if(isPartnerHere){
+        setPeer(new SimplePeer({
+          initiator:true,
+          trickle:false,
+          stream:stream,
+        }))
+      }
     })
-
-    peer.on("stream", stream => {
-      partnerVideo.current.srcObject = stream;
-    });
-
-    peer.signal(callerSignal);
-  }
-
+  
+   
+  
+    
+  }, []);
+  
+  
+  
   let UserVideo;
   if (stream) {
     UserVideo = (
@@ -137,43 +89,19 @@ function VideoChatTwo() {
   }
 
   let PartnerVideo;
-  if (callAccepted) {
+  if (isPartnerHere) {
     PartnerVideo = (
       <Video playsInline ref={partnerVideo} autoPlay />
     );
+  }else{
+    PartnerVideo = (<h2> Waiting for a partner...</h2>)
   }
-
-  let incomingCall;
-  if (receivingCall) {
-    incomingCall = (
-      <div>
-        <h1>{caller} is calling you</h1>
-        <button onClick={acceptCall}>
-          Accept
-        </button>
-      </div>
-    )
-  }
+  
   return (
     <Container>
       <Row>
         {UserVideo}
         {PartnerVideo}
-      </Row>
-      <Row>
-        {Object.keys(users).map(key => {
-          if (key === yourID) {
-            return null;
-          }
-          return (
-            <button onClick={() => callPeer(key)}>
-            Call {key}
-            </button>
-          );
-        })}
-      </Row>
-      <Row>
-        {incomingCall}
       </Row>
     </Container>
   );
