59a580ee1e4ff63fc88eae2ee5bab72f01de8636
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-05-28 13:22:05 -0400

    Moves app.get to the bottom of the use statements

--- app.js
+++ app.js
@@ -19,15 +19,14 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'client/build')))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
-
 app.use(userRouter)
 app.use(authenticate)
 app.use(rsvpRouter)
 app.use(categoryRouter)
 app.use(eventRouter)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 const rooms = {}
 
