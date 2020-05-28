10f8663f5da2a673b548038ae9bfa0a50f6aa1e7
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-05-27 23:35:17 -0400

    Add build files

---
+++ client/build/precache-manifest.94d99c3d628d5b4e6599a4436944658c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4abb7ddf276cd95e02238506bcc6f0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a693a7f8c2ea9473878",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a8be21d2dc9ac9e9ac8f",
+    "url": "/static/css/main.6010a64f.chunk.css"
+  },
+  {
+    "revision": "4a693a7f8c2ea9473878",
+    "url": "/static/js/2.8c8063a7.chunk.js"
+  },
+  {
+    "revision": "3aa47033bc784e556501b5ed71795669",
+    "url": "/static/js/2.8c8063a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8be21d2dc9ac9e9ac8f",
+    "url": "/static/js/main.01e1e4a1.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "bcd8140a210fb50f5baf8d29a407e400",
+    "url": "/static/media/navy-logo.bcd8140a.png"
+  },
+  {
+    "revision": "b5ecbc2c855bcefc32dc2caa1de85479",
+    "url": "/static/media/people.b5ecbc2c.jpg"
+  }
+]);
