35030a98a2c2c7653f33b0ee4176d82ced63f2f1
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-05-28 00:33:28 -0400

    Database connection string

--- db.js
+++ db.js
@@ -1,11 +1,21 @@
 const { Pool } = require('pg')
+const connection = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+}
 
-const pool = new Pool({
+const connectionLocal = {
   user: process.env.USER,
   host: process.env.HOST,
   database: process.env.DATABASE,
   port: process.env.DB_PORT
-})
+}
+
+const pool = new Pool(
+  process.env.NODE_ENV === 'production' ? connection : connectionLocal
+)
 
 module.exports = {
   query (text, params) {
